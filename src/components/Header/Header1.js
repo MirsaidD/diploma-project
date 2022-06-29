4bdb6c1eb60af0f8004d109c04883f6f07b2b280
MirsaidD <dzumaevmirsaid25@gmail.com>
2022-06-29 14:23:23 +0600

    add

---
+++ src/components/Header/Header1.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { CartBlock } from "../CartBlock";
+import Search from "../Search/Search";
+// import Inputs from "../Inputs/Inputs";
+import "./Header.css";
+
+export const Header = ({ searchValue, setSearchValue }) => {
+  return (
+    <div className="header">
+      <div className="wrapper">
+        <Link to="/" className="header__store-title">
+          Game Store
+        </Link>
+
+      </div>
+      <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+      <div className="wrapper header__cart-btn-wrapper">
+        <CartBlock />
+      </div>
+    </div>)
+}
